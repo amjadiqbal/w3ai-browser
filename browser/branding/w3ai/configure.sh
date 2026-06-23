8d7485be0783a28e2835adcb2f10a4f8e64c118e
Amjad <amjad1503d@aptechgdn.net>
2026-06-23 20:16:08 +0500

    chore(release): bump version to 1.0.7

--- browser/branding/w3ai/configure.sh
+++ browser/branding/w3ai/configure.sh
@@ -8,5 +8,5 @@ MOZ_MACBUNDLE_ID=org.w3ai.browser
 MOZ_CRASHREPORTER_URL=https://crash-reports.plato.ai
 
 # Override the artifact build's 151.0a1 version with our release version
-MOZ_APP_VERSION=1.0.6
-MOZ_APP_VERSION_DISPLAY=1.0.6
+MOZ_APP_VERSION=1.0.7
+MOZ_APP_VERSION_DISPLAY=1.0.7
