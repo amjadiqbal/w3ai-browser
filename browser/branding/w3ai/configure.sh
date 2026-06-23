aa93bbb90a63ef06b29c49c9a03aa981c2d0f76d
Amjad <amjad1503d@aptechgdn.net>
2026-06-24 00:29:33 +0500

    chore(release): reset version to 1.0.0 (dev builds don't bump)

--- browser/branding/w3ai/configure.sh
+++ browser/branding/w3ai/configure.sh
@@ -8,5 +8,5 @@ MOZ_MACBUNDLE_ID=org.w3ai.browser
 MOZ_CRASHREPORTER_URL=https://crash-reports.plato.ai
 
 # Override the artifact build's 151.0a1 version with our release version
-MOZ_APP_VERSION=1.0.20260624
-MOZ_APP_VERSION_DISPLAY=1.0.20260624
+MOZ_APP_VERSION=1.0.0
+MOZ_APP_VERSION_DISPLAY=1.0.0
